db1c71be686d483ecdb09588e8e5d57dde3c0319
mnptrinity <mohanprogramer@gmail.com>
2019-02-16 23:24:51 +0530

    first commit

---
+++ send_mail.php
@@ -0,0 +1,52 @@
+<?php
+require 'master/PHPMailerAutoload.php'; 
+if(isset($_POST['send']))
+        {
+            $name=$_POST['name'];
+            $mail=$_POST['email'];
+            $mailto = $mail;
+            $mailSub = "This also you can get it from the form page...";
+            $mailMsg = "hello this is the temporary mail .....".$name;
+            $mail = new PHPMailer();
+            $mail ->IsSmtp();
+            $mail ->SMTPDebug = 0;
+            $mail ->SMTPAuth = true;
+            $mail ->SMTPSecure = 'ssl';
+            $mail ->Host = "smtp.gmail.com";
+            $mail ->Port = 465; // or 587
+            $mail ->IsHTML(true);
+            $mail ->Username = "mohan2034677@gmail.com";
+            $mail ->Password = "9976764848";
+            $mail ->SetFrom("mohan2034677@gmail.com");
+            $mail ->Subject = $mailSub;
+            $mail ->Body = $mailMsg;
+            $mail ->AddAddress($mailto);
+
+            if($mail->Send())
+             {
+                 echo "<script>";
+                 echo "alert('Mail Sent successfully....');";
+                 echo "window.location.href='index.html';</script>";
+             }
+            else
+             {
+                  echo "<script>";
+                 echo "alert('Mail not Sent successfully....');";
+                 echo "window.location.href='index.html';</script>";              
+             }
+                    
+         }
+
+      else
+      {
+          echo "<script>alert('There is an error in the  form...');window.location.href=`index.html`;</script>";
+          header('Location : index.php');
+      }
+   
+    
+
+?>
+
+
+   
+
